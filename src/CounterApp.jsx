677c15b41e5f289a613ceca7909b402b32cc79f2
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-12-19 20:21:41 -0500

    useState uso

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,29 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 
 export const CounterApp = ({ value }) => {
+
+  //Primer hook useState
+  const [counter, setCounter] = useState(value) //sirve para modificar el estado de algo, tiene un valor inicial
+
   //funciones
   function sumarUno(event) {
-    console.log(event);
+    setCounter(counter + 1)
+
+    // setCounter((c) => c+1) otra forma de hacerlo
   }
+
+//   lo mismo pero como arrow function
+//   const sumarUno = (event) => {
+//     console.log(event);
+//   }
 
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+
+      {/* uso el counter del useState */}
+      <h2>{counter}</h2>
 
       {/* evento onClick */}
       <button onClick={(event) => sumarUno(event)}>+1</button>
