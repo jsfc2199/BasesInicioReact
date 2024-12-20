b97ceaaa5eabf13373dd72a8d0bfbb7209b22b1f
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-12-19 19:15:39 -0500

    Nuevo componente y retornar un fragment

---
+++ src/FirstApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const FirstApp = () => {
+  return (
+    // si mandamos <> </> es un fragment
+    //también se puede usar <Fragment></Fragment>
+    <>
+      <div>FirstApp</div>
+      <p>párrafo</p>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@
 //la app de react siempre cuenta con estos dos imports
 import React from "react";
 import ReactDOM from "react-dom/client";
+import HelloWorldApp from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
-// import FirstApp from "./FirstApp";
 // import CounterApp from "./CounterApp";
 
 import './styles.css'
-import HelloWorldApp from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //miramos que el index.html tiene un id root, allí se renderiza todo y renderizamos a App
   <React.StrictMode>
     <HelloWorldApp/>
+    <FirstApp/>
   </React.StrictMode>
 );
