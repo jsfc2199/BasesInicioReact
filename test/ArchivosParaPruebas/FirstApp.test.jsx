90ff21208db315d75ac99beb25d528e46f3cb587
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-01-15 11:14:54 -0500

    pruebas de snapshot en first app

--- test/ArchivosParaPruebas/FirstApp.test.jsx
+++ test/ArchivosParaPruebas/FirstApp.test.jsx
@@ -1,12 +1,32 @@
-import { render } from "@testing-library/react"
-import { FirstApp } from "../../src/FirstApp"
+import { render } from "@testing-library/react";
+import { FirstApp } from "../../src/FirstApp";
 
+describe("Pruebas en first app", () => {
+  test("Debe hacer match con el snapshot", () => {
+    const title = "Hola";
+    const subtitle = 123;
 
-describe('Pruebas en first app', () => {
-  test('Debe hacer match con el snapshot', () => {
+    const { container } = render(
+      <FirstApp title={title} subtitle={subtitle} />
+    );
 
-    //render rendereiza el componente en memora
-    render(<FirstApp />)
-  })
+    expect(container).toMatchSnapshot(); //foto de lo que hay
+  });
+
   
-})
+  test("Debe de mostrar el titulo en un h2", () => {
+    const title = "Hola";
+    const subtitle = 123;
+
+    const { container, getByText } = render(
+      <FirstApp title={title} subtitle={subtitle} />
+    );
+
+    expect(getByText(title)).toBeTruthy()
+
+    //no recomendado
+    const h1 = container.querySelector('h2')
+    expect(h1.innerHTML).toBe(title)
+
+  });
+});
