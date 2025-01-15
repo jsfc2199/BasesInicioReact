a0cae327b97b86c90ee3c500fe6a395ba6e5fa1c
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-01-15 15:02:40 -0500

    pruebas simulando evento click

--- test/ArchivosParaPruebas/CounterApp.test.jsx
+++ test/ArchivosParaPruebas/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "./../../src/CounterApp";
 
 describe("Pruebas en el counter app", () => {
@@ -13,4 +13,31 @@ describe("Pruebas en el counter app", () => {
     render(<CounterApp value={100} />);
     expect(screen.getByText(100)).toBeTruthy();
   });
+
+  //Simulaciones de evento click
+  test("Debe de incrementar con el boton +1", () => {
+    render(<CounterApp value={value} />);
+    //fire event de react testing library
+    fireEvent.click(screen.getByText("+1")); //+1 porque el componente tiene un +1 en el boton
+    expect(screen.getByText("124")).toBeTruthy();
+  });
+
+  test("Debe de decrementar con el boton -1", () => {
+    render(<CounterApp value={value} />);
+    fireEvent.click(screen.getByText("-1"));
+    screen.debug();
+    expect(screen.getByText("122")).toBeTruthy();
+  });
+
+  test('Debe de resetear el contador', () => {
+    render(<CounterApp value={value}/>)   
+    fireEvent.click(screen.getByText('-1'))
+    fireEvent.click(screen.getByText('-1'))
+    fireEvent.click(screen.getByText('-1'))
+    fireEvent.click(screen.getByText('-1'))
+    fireEvent.click(screen.getByText('reset'))
+    screen.debug()
+    expect(screen.getByText(123)).toBeTruthy()
+  })
+
 });
